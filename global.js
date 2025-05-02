5c761aa78afb0b3a5ac8e2d958a68edc27de0b2e
Ahmad Ageel <129811017+AhmadAgeel@users.noreply.github.com>
2025-05-01 23:32:35 -0700

    proj2 and half lab4

--- global.js
+++ global.js
@@ -102,3 +102,40 @@ form?.addEventListener(
 );
 
 // mailto:leaverou@mit.edu?subject=Hello&body=Sup?
+
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+// export function renderProjects(project, containerElement) {
+//   containerElement.innerHTML = '';
+//   const article = document.createElement('article');
+//   article.innerHTML = `
+//     <h3>${project.title}</h3>
+//     <img src="${project.image}" alt="${project.title}">
+//     <p>${project.description}</p>
+//   `;
+//   containerElement.appendChild(article);
+
+// }
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // write javascript that will allow dynamic heading levels based on previous function
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+
+}
